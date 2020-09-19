7913c421a49a5032631c33158a74a6a7c4c19b3d
Abhi <onelife1906@gmail.com>
2020-09-19 21:35:26 +0530

    Day 3

---
+++ Assignment Day3/Day3.js
@@ -0,0 +1,12 @@
+function myFunction() {
+	document.getElementById("button").style.display ="none";
+	document.getElementById("button1").style.display ="block";
+	document.getElementById("box").style.display = "block";
+	document.getElementById("box1").style.display = "none";
+}		
+function myFunction1() {
+	document.getElementById("button1").style.display ="none";
+	document.getElementById("button").style.display ="block";
+	document.getElementById("box").style.display = "none";
+	document.getElementById("box1").style.display = "block";
+  }		
